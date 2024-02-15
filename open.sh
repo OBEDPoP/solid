ef786a0b39c5df414eac302bfdc304f7275099ef
OBED <159868574+OBEDPoP@users.noreply.github.com>
2024-02-15 15:44:39 +0530

    Update open.sh

--- open.sh
+++ open.sh
@@ -25,10 +25,10 @@ sudo apt install -y keystone
 sudo cp /etc/keystone/keystone.conf /etc/keystone/keystone.conf.orig
 
 # Configure Keystone database
-sudo crudini --set /etc/keystone/keystone.conf database connection "mysql+pymysql://keystone:0penstack@localhost/keystone"
+sudo crudini --set /etc/keystone/keystone.conf database connection "mysql+pymysql://keystone:0penstack@open/keystone"
 
 # Configure RabbitMQ
-sudo crudini --set /etc/keystone/keystone.conf oslo_messaging_rabbit rabbit_host localhost
+sudo crudini --set /etc/keystone/keystone.conf oslo_messaging_rabbit rabbit_host open
 sudo crudini --set /etc/keystone/keystone.conf oslo_messaging_rabbit rabbit_userid openstack
 sudo crudini --set /etc/keystone/keystone.conf oslo_messaging_rabbit rabbit_password 0penstack
 
@@ -37,9 +37,9 @@ sudo su -s /bin/sh -c "keystone-manage db_sync" keystone
 sudo su -s /bin/sh -c "keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone" keystone
 sudo su -s /bin/sh -c "keystone-manage credential_setup --keystone-user keystone --keystone-group keystone" keystone
 sudo su -s /bin/sh -c "keystone-manage bootstrap --bootstrap-password 0penstack \
-  --bootstrap-admin-url http://localhost:5000/v3/ \
-  --bootstrap-internal-url http://localhost:5000/v3/ \
-  --bootstrap-public-url http://localhost:5000/v3/ \
+  --bootstrap-admin-url http://open:5000/v3/ \
+  --bootstrap-internal-url http://open:5000/v3/ \
+  --bootstrap-public-url http://open:5000/v3/ \
   --bootstrap-region-id RegionOne" keystone
 
 # Configure Apache for Keystone
@@ -47,9 +47,9 @@ sudo cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.orig
 sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.orig
 sudo cp /etc/apache2/sites-available/wsgi-keystone.conf /etc/apache2/sites-available/wsgi-keystone.conf.orig
 
-sudo crudini --set /etc/apache2/apache2.conf Global ServerName localhost
-sudo crudini --set /etc/apache2/sites-available/000-default.conf VirtualHost "ServerName localhost\nServerAdmin webmaster@localhost"
-sudo crudini --set /etc/apache2/sites-available/wsgi-keystone.conf VirtualHost "ServerName localhost"
+sudo crudini --set /etc/apache2/apache2.conf Global ServerName open
+sudo crudini --set /etc/apache2/sites-available/000-default.conf VirtualHost "ServerName open\nServerAdmin webmaster@open"
+sudo crudini --set /etc/apache2/sites-available/wsgi-keystone.conf VirtualHost "ServerName open"
 
 sudo service apache2 restart
 
@@ -59,17 +59,17 @@ export OS_PASSWORD=0penstack
 export OS_PROJECT_NAME=admin
 export OS_USER_DOMAIN_NAME=Default
 export OS_PROJECT_DOMAIN_NAME=Default
-export OS_AUTH_URL=http://localhost:5000/v3
+export OS_AUTH_URL=http://open:5000/v3
 export OS_IDENTITY_API_VERSION=3
 
 # Install and configure Glance
 sudo apt install -y glance
 sudo cp /etc/glance/glance-api.conf /etc/glance/glance-api.conf.orig
 
-sudo crudini --set /etc/glance/glance-api.conf database connection "mysql+pymysql://glance:0penstack_@localhost/glance"
-sudo crudini --set /etc/glance/glance-api.conf keystone_authtoken auth_uri "http://localhost:5000"
-sudo crudini --set /etc/glance/glance-api.conf keystone_authtoken auth_url "http://localhost:5000"
-sudo crudini --set /etc/glance/glance-api.conf keystone_authtoken memcached_servers "localhost:11211"
+sudo crudini --set /etc/glance/glance-api.conf database connection "mysql+pymysql://glance:0penstack_@open/glance"
+sudo crudini --set /etc/glance/glance-api.conf keystone_authtoken auth_uri "http://open:5000"
+sudo crudini --set /etc/glance/glance-api.conf keystone_authtoken auth_url "http://open:5000"
+sudo crudini --set /etc/glance/glance-api.conf keystone_authtoken memcached_servers "open:11211"
 sudo crudini --set /etc/glance/glance-api.conf keystone_authtoken auth_type "password"
 sudo crudini --set /etc/glance/glance-api.conf keystone_authtoken project_domain_name "Default"
 sudo crudini --set /etc/glance/glance-api.conf keystone_authtoken user_domain_name "Default"
@@ -95,12 +95,12 @@ openstack image create \
 sudo apt install -y nova-api nova-conductor nova-consoleauth nova-novncproxy nova-scheduler nova-placement-api nova-compute
 sudo cp /etc/nova/nova.conf /etc/nova/nova.conf.orig
 
-sudo crudini --set /etc/nova/nova.conf database connection "mysql+pymysql://nova:0penstack@localhost/nova"
-sudo crudini --set /etc/nova/nova.conf DEFAULT transport_url "rabbit://openstack:0penstack@localhost"
+sudo crudini --set /etc/nova/nova.conf database connection "mysql+pymysql://nova:0penstack@open/nova"
+sudo crudini --set /etc/nova/nova.conf DEFAULT transport_url "rabbit://openstack:0penstack@open"
 sudo crudini --set /etc/nova/nova.conf DEFAULT my_ip "10.0.0.1"
-sudo crudini --set /etc/nova/nova.conf keystone_authtoken auth_uri "http://localhost:5000"
-sudo crudini --set /etc/nova/nova.conf keystone_authtoken auth_url "http://localhost:5000"
-sudo crudini --set /etc/nova/nova.conf keystone_authtoken memcached_servers "localhost:11211"
+sudo crudini --set /etc/nova/nova.conf keystone_authtoken auth_uri "http://open:5000"
+sudo crudini --set /etc/nova/nova.conf keystone_authtoken auth_url "http://open:5000"
+sudo crudini --set /etc/nova/nova.conf keystone_authtoken memcached_servers "open:11211"
 sudo crudini --set /etc/nova/nova.conf keystone_authtoken auth_type "password"
 sudo crudini --set /etc/nova/nova.conf keystone_authtoken project_domain_name "Default"
 sudo crudini --set /etc/nova/nova.conf keystone_authtoken user_domain_name "Default"
